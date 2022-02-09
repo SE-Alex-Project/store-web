a572ca28c657306581dd2dabe50a709dbeed0dbd
yousefhassan1999 <yousefhassan121999@gmail.com>
2022-02-09 04:53:20 +0200

    done

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="cart">
-    <!-- <cart-item /> -->
     <Cartitemnew />
   </div>
 </template>
 <script>
-// import cartItem from "@/components/cartItem.vue";
 import Cartitemnew from "@/components/cartitemnew.vue";
 export default {
   name: "cart",
   components: {
-    // cartItem,
     Cartitemnew,
   },
   mounted() {
