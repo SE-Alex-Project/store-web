9093f3dc26fdb24f34b5f66b231ce99b150b7fa7
yousefhassan1999 <yousefhassan121999@gmail.com>
2022-02-09 14:47:08 +0200

    done

--- src/components/MangerCartComp.vue
+++ src/components/MangerCartComp.vue
@@ -7,37 +7,18 @@
         <div class="box" v-for="(empl, index) in empls" :key="index">
           <div class="product">
             <div class="nameCont">
+              <div class="NumdeCont">
+                <span class="title">Cart ID</span>
+                <h4 class="productname">{{ empl.cartId }}</h4>
+              </div>
               <div class="firstName">
-                <span class="title">First Name</span>
-                <h4 class="productname">{{ empl.fName }}</h4>
+                <span class="title">Email</span>
+                <h4 class="productname">{{ empl.userEmail }}</h4>
               </div>
               <div class="secondName">
-                <span class="title">Second Name</span>
-                <h4 class="productname">{{ empl.lName }}</h4>
+                <span class="title">Date</span>
+                <h4 class="productname">{{ empl.buyDate }}</h4>
               </div>
-              <div class="NumdeCont">
-                <span class="title">Store Number</span>
-                <h4 class="productname">{{ empl.storeId }}</h4>
-              </div>
-            </div>
-            <div class="SECont">
-              <div>
-                <span class="title">salary</span>
-                <h4 class="Email">{{ empl.salary }}</h4>
-              </div>
-              <div>
-                <span class="title">Role</span>
-                <h4 class="Email">{{ empl.erole }}</h4>
-              </div>
-            </div>
-            <div class="EmailCont">
-                <span class="title">Email</span>
-                <h4 class="Email">{{ empl.email }}</h4>
-            </div>
-            <div class="buttoncontainer2">
-              <span class="iconcon" @click="delete_emp(empl)">
-                <i class="fas fa-user-times"></i>
-              </span>
             </div>
           </div>
         </div>
@@ -58,7 +39,7 @@ export default {
   mounted() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    var raw = JSON.stringify(this.$store.state.email_crt);
+    var raw = this.$store.state.email_crt;
     var requestOptions = {
       method: "POST",
       headers: myHeaders,
@@ -74,49 +55,7 @@ export default {
       })
       .catch((error) => console.log("error", error));
   },
-  methods: {
-    delete_emp: function (empl) {
-      console.log(empl);
-
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      var raw = JSON.stringify({
-        token: window.sessionStorage.getItem("token"),
-        email: empl.email,
-      });
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
-      fetch("http://127.0.0.1:8080/user/removeAccount", requestOptions)
-        .then((response) => response.text())
-        .then((result) => {
-          console.log(result);
-          this.get_emp();
-        })
-        .catch((error) => console.log("error", error));
-    },
-    get_emp: function () {
-      var myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      var raw = window.sessionStorage.getItem("token");
-      var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-      };
-      fetch("http://127.0.0.1:8080/manager/getEmployees", requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-          console.log(result);
-          this.empls = result;
-        })
-        .catch((error) => console.log("error", error));
-    },
-  },
+  methods: {},
 };
 </script>
 
@@ -157,7 +96,7 @@ export default {
 }
 .box {
   width: 100%;
-  min-height: 200px;
+  min-height: 100px;
   background: #eff3f3;
   border: 3px rgb(9, 205, 240);
   box-shadow: 0px 0px 35px rgb(166, 169, 170);
@@ -210,41 +149,5 @@ h4 {
   .title {
     font-size: 10px;
   }
-}
-.EmailCont {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 100%;
-}
-
-.SECont{
-display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 100%;
-}
-.buttoncontainer2 {
-  padding: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.iconcon {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 60px;
-  height: 60px;
-}
-.fa-user-times {
-  font-size: 40px;
-  color: black;
-}
-.fa-user-times:hover {
-  cursor: pointer;
-  color: var(--main-color);
 }
 </style>
